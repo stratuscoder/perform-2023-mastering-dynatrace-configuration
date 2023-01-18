2fdb51e1d3a38c47e31cba685cb952d521affa7b
Gary Carr <gary.carr@dynatrace.com>
2023-01-18 16:45:04 -0600

    dry run updates

--- scripts/describe-pod.sh
+++ scripts/describe-pod.sh
@@ -1,1 +1,1 @@
-sh get-pod-name.sh $1|awk {'system("kubectl -n easytrade describe pod " $1)'}
+sh ~/perform-2023-mastering-dynatrace-configuration/scripts/get-pod-name.sh $1|awk {'system("kubectl -n easytrade describe pod " $1)'}
--- scripts/metadata-viewer.sh
+++ scripts/metadata-viewer.sh
@@ -1,1 +1,2 @@
-kubectl -n easytrade create -f ../easytrade/dynatrace-oneagent-metadata-viewer.yaml
+cd ~/perform-2023-mastering-dynatrace-configuration
+kubectl -n easytrade create -f easytrade/dynatrace-oneagent-metadata-viewer.yaml
