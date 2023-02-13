db20e8a7c9b32b69167ad056c13dc99a18a5297e
Gary Carr <gary.carr@dynatrace.com>
2023-02-13 12:19:07 -0800

    cleaned up yaml updates.

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,3 +1,4 @@
 rm -fr ~/perform-2023-mastering-dynatrace-configuration
+cd ~
 git clone https://github.com/stratuscoder/perform-2023-mastering-dynatrace-configuration.git
 ls ~/perform-2023-mastering-dynatrace-configuration
