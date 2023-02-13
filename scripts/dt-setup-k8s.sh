5984172b8e02af5d447a18a4e09800ab6c27fad1
Gary Carr <gary.carr@dynatrace.com>
2023-02-13 11:38:15 -0800

    cleaned up yaml updates.

--- scripts/dt-setup-k8s.sh
+++ scripts/dt-setup-k8s.sh
@@ -1,13 +1,3 @@
 chmod 775 ~/perform-2023-mastering-dynatrace-configuration/easytrade 
 cd ~/perform-2023-mastering-dynatrace-configuration/easytrade 
-cp -f ~/deploy/easytrade/manifests/pricingservice.yaml pricingservice.yamlv1
-awk 'NR<=28 {print}' pricingservice.yamlv1 > pricingservice.yamlv2
-cat pricingservice.yamlv2partial >> pricingservice.yamlv2
-awk 'NR>28 {print}' pricingservice.yamlv1 >> pricingservice.yamlv2
-awk 'NR<=9 {print}' pricingservice.yamlv2 > pricingservice.yamlv3
-cat pricingservice.yamlv3partial >> pricingservice.yamlv3
-awk 'NR>9 {print}' pricingservice.yamlv2 >> pricingservice.yamlv3
-awk 'NR<=31 {print}' pricingservice.yamlv3 > pricingservice.yamlv4
-cat pricingservice.yamlv4partial >> pricingservice.yamlv4
-awk 'NR>31 {print}' pricingservice.yamlv3 >> pricingservice.yamlv4
 echo "Setup has completed."
---
+++ scripts/refresh.sh
@@ -0,0 +1,3 @@
+rm -fr ~/perform-2023-mastering-dynatrace-configuration
+git clone https://github.com/stratuscoder/perform-2023-mastering-dynatrace-configuration.git
+ls ~/perform-2023-mastering-dynatrace-configuration
